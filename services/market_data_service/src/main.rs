340669521e34ba0acb1aced1c0f15599ed16fb96
nirdesh-dev <nbhattarai.work@gmail.com>
2025-07-16 13:11:19 +0545

    WIP: Download options data from IBKR

--- services/market_data_service/src/main.rs
+++ services/market_data_service/src/main.rs
@@ -1,4 +1,15 @@
 mod ib_client;
-fn main() {
-    println!("Hello, world!");
+
+use anyhow::Result;
+use ib_client::IBClient;
+
+use crate::ib_client::IBConfig;
+
+fn main() -> Result<()> {
+    let ib = IBClient::new(IBConfig::default());
+    let mut client = ib.connect()?;
+
+    ib.get_spx_minute_data(&mut client)?;
+
+    Ok(())
 }
