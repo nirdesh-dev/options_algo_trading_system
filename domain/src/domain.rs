d6603862b11d48d26d35b469370c3430c06c0d33
nirdesh-dev <nbhattarai.work@gmail.com>
2025-08-06 05:15:53 +0545

    file/folder restructuirng

---
+++ domain/src/domain.rs
@@ -0,0 +1,24 @@
+use chrono::{DateTime, Local, NaiveDate};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Quote {
+    pub symbol: String,
+    pub bid: f64,
+    pub ask: f64,
+    pub bid_size: u32,
+    pub ask_size: u32,
+    pub biddate: DateTime<Local>,
+    pub askdate: DateTime<Local>,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct Candle {
+    pub symbol: Option<String>,
+    // pub date: NaiveDate,
+    pub open: f64,
+    pub high: f64,
+    pub low: f64,
+    pub close: f64,
+    pub volume: i64,
+}
